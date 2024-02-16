cefdca5b44dff5cf767015fe33a956a4781fdc36
DESKTOP-BI5BBNV\hi <stv9450@gmail.com>
2024-02-16 11:50:19 +0900

    first commit

---
+++ src/main/java/org/example/ohgiraffers/pr01/Pr01Application.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.pr01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr01Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ohgiraffers/pr01/Pr01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.pr01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
